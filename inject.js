6d488f16f6d5263e92e1776b5989a622f28e8b20
Nando Vieira <fnando.vieira@gmail.com>
2017-08-23 12:54:19 -0700

    Automatically skip ads.

--- inject.js
+++ inject.js
@@ -1,23 +1,47 @@
-(function(){
+(function YoutubeAdsExtension(){
   if (window.top !== window) {
     return;
   }
 
+  if (!video) {
+    setTimeout(YoutubeAdsExtension, 100);
+    return;
+  }
+
+  function getVideo() {
+    return document.querySelector('video');
+  }
+
   function skipYoutubeAds() {
-    var video = document.querySelector('video');
+    var video = getVideo();
     video.currentTime = video.duration;
   }
 
+  function autoSkipAds() {
+    var element = document.querySelector('.ad-showing');
+
+    if (element) {
+      skipYoutubeAds();
+    }
+  }
+
   safari.self.addEventListener('message', function(event) {
-    skipYoutubeAds();
+    setTimeout(skipYoutubeAds, 200);
   }, false);
 
   window.addEventListener('keydown', function(event) {
     var trigger = event.shiftKey && event.metaKey && event.which === 89; // cmd-shift-Y
 
-
     if (trigger) {
       skipYoutubeAds();
     }
   }, false);
+
+  setInterval(function(){
+    var video = getVideo();
+
+    if (video.currentTime > 0) {
+      autoSkipAds();
+    }
+  }, 1000);
 })();
